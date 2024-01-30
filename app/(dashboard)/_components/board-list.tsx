f2d63d8eb4dc10fae34da9716c96ea85d0712718
TadevosyannGarik <tadevosyan12@gmail.com>
2024-01-30 22:37:51 +0400

    Board List

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -3,6 +3,10 @@
 import { EmptyBoards } from "./empty-boards";
 import { EmptyFavorites } from "./empty-favorites";
 import { EmptySearch } from "./empty-search";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 
 interface BoardListProps {
@@ -14,23 +18,58 @@ interface BoardListProps {
 };
 
 export const BoardList = ({orgId, query}: BoardListProps) => {
-    const data = [];
+    const data = useQuery(api.boards.get, { orgId });
 
-    if (!data.length && query.search) {
+    if (data === undefined) {
+        return (
+            <div>
+                <h2 className="text-3xl">
+                    {query.favorites ? "Favorite boards" : "Team boards"}
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                    <NewBoardButton orgId={orgId} disabled />
+                    <BoardCard.Skeleton />
+                    <BoardCard.Skeleton />
+                    <BoardCard.Skeleton />
+                    <BoardCard.Skeleton />
+                </div>
+          </div>
+        )
+    }
+
+    if (!data?.length && query.search) {
         return <EmptySearch />;
     }
 
-    if (!data.length && query.favorites) {
+    if (!data?.length && query.favorites) {
         return <EmptyFavorites />;
     }
 
-    if (!data.length) {
+    if (!data?.length) {
         return <EmptyBoards />
     }
 
     return (
         <div>
-            {JSON.stringify(query)}
+            <h2 className="text-3xl">
+                {query.favorites ? "Favorites Boards": "Team Boards"}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                <NewBoardButton orgId={orgId} />
+                {data?.map((board) => (
+                    <BoardCard 
+                        key={board._id}
+                        id={board._id}
+                        title={board.title}
+                        imageUrl={board.imageUrl}
+                        authorId={board.authorId}
+                        authorName={board.authorName}
+                        createdAt={board._creationTime}
+                        orgId={board.orgId}
+                        isFavorite={false}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
