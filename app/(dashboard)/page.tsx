42815b21c37fa3236899b4860b05b3c77d5d5791
TadevosyannGarik <tadevosyan12@gmail.com>
2024-01-30 19:02:01 +0400

    Empty States

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,30 @@
-const DashboardPage = () => {
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrganization } from "./_components/empty-organization";
+import { BoardList } from "./_components/board-list";
+
+
+interface DashboardPageProps {
+    searchParams: {
+        search?: string;
+        favorites?: string
+    };
+};
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+    const { organization } = useOrganization();
+    
     return (
-        <div>
-            Dashboard Root Page
+        <div className="flex-1 h-[calc(100%-80px)] p-6">
+            {!organization ? (
+                <EmptyOrganization />
+            ) : (
+                <BoardList 
+                    orgId={organization.id}
+                    query={searchParams}
+                />
+            )}
         </div>
     );
 };
