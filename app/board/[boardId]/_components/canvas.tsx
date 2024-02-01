40135aa2c5816c844e1ceee0bade7f4bd0104c46
TadevosyannGarik <tadevosyan12@gmail.com>
2024-02-01 17:03:32 +0400

    Canvas State

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useHistory, useCanRedo, useCanUndo } from "@/liveblocks.config";
+import { CanvasMode, CanvasState } from "@/types/canvas";
+import { useState } from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useSelf } from "@/liveblocks.config";
 
 
 interface CanvasProps {
@@ -11,13 +13,26 @@ interface CanvasProps {
 };
 
 export const Canvas = ({ boardId }: CanvasProps) => {
-    const info = useSelf((me) => me.info)
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None,
+    });
+
+    const history = useHistory();
+    const canRedo = useCanRedo();
+    const canUndo = useCanUndo();
 
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info boardId={boardId} />
             <Participants />
-            <Toolbar />
+            <Toolbar 
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                redo={history.redo}
+                undo={history.undo}
+            />
         </main>
     );
 };
